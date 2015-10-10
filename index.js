eecdbec76e6a4a89c49007062022c4606469e087
Chris Caragianis <chriscaragianis@gmail.com>
2015-10-10 14:20:53 -0400

    Hook up form to db.  Technically this is now a full stack app.  Woo.

---
+++ index.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database('address_book.db');
+
+var app = express();
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/', function (req, res) {
+  res.render('public/index.html');
+});
+
+app.post('/postData', function (req, res) {
+  db.run("insert into Persons (FirstName, LastName, Address, City, State, Zip) values (?,?,?,?,?,?)", req.body.fname , req.body.lname, req.body.address,  req.body.city, req.body.state, req.body.zip);
+  res.send('Data ok! Sent. Farted.');
+});
+
+var server = app.listen(3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+});
